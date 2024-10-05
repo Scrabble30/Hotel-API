01b83366eac63f344483ea9d23529850fd91f95d
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2024-10-05 11:14:32 +0200

    Add new code

--- src/main/java/app/security/controllers/AccessController.java
+++ src/main/java/app/security/controllers/AccessController.java
@@ -1,9 +1,9 @@
 package app.security.controllers;
 
-import app.exceptions.APIException;
+import app.security.enums.Role;
 import dk.bugelhartmann.UserDTO;
 import io.javalin.http.Context;
-import io.javalin.http.HttpStatus;
+import io.javalin.http.UnauthorizedResponse;
 import io.javalin.security.RouteRole;
 import jakarta.persistence.EntityManagerFactory;
 
@@ -22,16 +22,27 @@ public class AccessController implements IAccessController {
      *
      * @param ctx
      */
-    @Override
     public void accessHandler(Context ctx) {
-        UserDTO user = ctx.attribute("user"); // the User was put in the context by the SecurityController.authenticate method (in a before filter on the route)
+
+        // If no roles are specified on the endpoint, then anyone can access the route
+        if (ctx.routeRoles().isEmpty() || ctx.routeRoles().contains(Role.ANYONE)) {
+            return;
+        }
+
+        // Check if the user is authenticated
+        try {
+            securityController.authenticate().handle(ctx);
+        } catch (UnauthorizedResponse e) {
+            throw new UnauthorizedResponse(e.getMessage());
+        } catch (Exception e) {
+            throw new UnauthorizedResponse("You need to log in, dude! Or you token is invalid.");
+        }
+
+        // Check if the user has the necessary roles to access the route
+        UserDTO user = ctx.attribute("user");
         Set<RouteRole> allowedRoles = ctx.routeRoles(); // roles allowed for the current route
         if (!securityController.authorize(user, allowedRoles)) {
-            if (user != null) {
-                throw new APIException(HttpStatus.FORBIDDEN.getCode(), "Unauthorized with roles: " + user.getRoles() + ". Needed roles are: " + allowedRoles);
-            } else {
-                throw new APIException(HttpStatus.FORBIDDEN.getCode(), "You need to log in, dude!");
-            }
+            throw new UnauthorizedResponse("Unauthorized with roles: " + (user != null ? user.getRoles() : null) + ". Needed roles are: " + allowedRoles);
         }
     }
 }
